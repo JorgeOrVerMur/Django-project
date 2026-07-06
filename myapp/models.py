f8432094e15453f1e1c339ed986b416d3bb4984c
jorgeorlandovmur@gmail.com <jorgeorlandovmur@gmail.com>
2026-07-06 18:15:09 -0500

    feat: add done field to Task model and implement project and task display templates

--- myapp/models.py
+++ myapp/models.py
@@ -12,7 +12,8 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    done = models.BooleanField(default=False)
 
     def __str__(self):
         # return self.title
-        return f"{self.title} - {self.project.name}"
+        return f"{self.title} - {self.project.name} - {self.done}"
